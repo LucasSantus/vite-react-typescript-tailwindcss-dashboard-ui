6d4182e5299775e3c9ecd93610368a357de0f5f5
Lucas Santos <leos.developer@gmail.com>
2023-03-01 22:38:42 -0300

    :rocket: add config for deploy project

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -7,7 +7,7 @@ export const Score: React.FC = () => (
       <span className="font-inter font-semibold text-2xl text-white">
         NPS Geral
       </span>
-      <span className="font-inter font-semibold text-2xl text-custom-green-500 flex-col justify-center items-center gap-2">
+      <span className="font-inter font-semibold text-2xl text-custom-green-500 flex flex-col justify-center items-center gap-2">
         <IconHappyPerson />
         Excelente!
       </span>
